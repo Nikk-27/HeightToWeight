c258624027d1096609988532e2e85795ae64b401
nikk <nktllwn@gmail.com>
2021-03-18 17:46:50 +0530

    init

---
+++ HeightToWeight/H2W/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from django.core import serializers
+from django.conf import settings
+import json
+
+# Create your views here.
+@api_view(["POST"])
+def Weight(height):
+    try:
+        height = json.loads(height.body)
+        weight = str(height*10)
+
+        return JsonResponse("Ideal Weight should be:"+weight+" kilos", safe = False)
+    except ValueError as e:
+        return Response(e.args[0],status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(["POST"])
+def IdealWeight(heightdata):
+    try:
+        height=json.loads(heightdata.body)
+        weight=str(height*10)
+
+        return JsonResponse("Ideal weight should be:"+weight+" kg",safe=False)
+    except ValueError as e:
+        return Response(e.args[0],status.HTTP_400_BAD_REQUEST)
